d1fd660587da740d8c2cd5b1580f425c43e6604a
klagrange <keith@bambu.life>
2020-02-18 12:10:59 +0800

    clean

--- lambda_freezer/__init__.py
+++ lambda_freezer/__init__.py
@@ -459,11 +459,3 @@ def deploy(rest_api_id, region, version, stage_description, domain_name=None):
             stage=stage_name
         )
 
-if __name__ == "__main__":
-    REST_API_ID = "r5ltm5nqr7"
-    REGION = "us-east-1"
-    DOMAIN_NAME = "api-library.bambu.life"
-    run_after_default_deployment(REST_API_ID, REGION, DOMAIN_NAME)
-
-    # API_FNS = Mapper.run(REST_API_ID, REGION)
-    # deploy(REST_API_ID, REGION, "1.2.0", "some description!", DOMAIN_NAME)
